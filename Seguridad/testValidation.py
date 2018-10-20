d25c6243e5a7f2b8dafb91d097614fe706082c13
Constanza Abarca <costi.abarca@gmail.com>
2018-10-20 01:33:39 -0400

    Creadas pruebas de malicia

--- Seguridad/testValidation.py
+++ Seguridad/testValidation.py
@@ -50,5 +50,32 @@ class testValidation(unittest.TestCase):
     # Prueba para chequear que el registro falla si la contraseña le falta una letra
     def testFaltaLetra(self):
         self.assertEqual(self.validacion.registrarUsuario("costiabarca@gmail.com", "ab123456789", "ab123456789"), "Clave invalida")
+
+# Pruebas esquina
+    
+    def testPassIncorrecto(self):
+        self.validacion.registrarUsuario("denylson97@gmail.com", "pru3bA12", "pru3bA12")
+        self.assertEqual(self.validacion.ingresarUsuario("denylson97@gmail.com", "pru3bA1"), "Clave invalida")
+    
+    def testPassInvalido(self):
+        self.validacion.registrarUsuario("denylson97@gmail.com", "pru3bA12", "pru3bA12")
+        self.assertEqual(self.validacion.ingresarUsuario("denylson97@gmail.com", "incorrecT0"), "Clave invalida")
+    
+    def testDatosInv(self):
+        self.assertEqual(self.validacion.registrarUsuario("costiabarcagmai", "pr", "pr"), "Correo electronico invalido")
+    
+# Pruebas malicia
+    
+    def testYaExiste(self):
+        self.validacion.registrarUsuario("costi.abarca@gmail.com", "pru3bA12", "pru3bA12")
+        self.assertEqual(self.validacion.registrarUsuario("costi.abarca@gmail.com", "pru3b33A1", "pru3b33A1"), "Correo electronico invalido")
+    
+    def testRegistroVacio(self):
+        self.assertEqual(self.validacion.registrarUsuario("", "", ""), "Correo electronico invalido")
+    
+    def testIngresoVacio(self):
+        self.assertEqual(self.validacion.ingresarUsuario("", ""), "Correo electronico invalido")
+        
+        
         
         
