cda32898ee8b99e8555012a53453559a03a8e69a
Constanza Abarca <costi.abarca@gmail.com>
2018-10-19 23:38:48 -0400

    Creadas pruebas para chequear registro e ingreso valido. Fallan

--- Seguridad/testValidation.py
+++ Seguridad/testValidation.py
@@ -13,5 +13,14 @@ class testValidation(unittest.TestCase):
         self.validacion = Seguridad()
 
 # Pruebas interiores
-
-    def testCalculoSemanas(self):
+    
+    # Prueba para chequear que un usuario con datos validos se registra exitosamente
+    def testRegistroUser(self):
+        self.assertEqual(self.validacion.registrarUsuario("costi.abarca@gmail.com", "pru3bA1", "pru3bA1"), "Usuario aceptado")
+    
+    # Prueba para chequear que un usuario registro puede acceder al sistema
+    def testIngresoUser(self):
+        self.validacion.registrarUsuario("costi.abarca@gmail.com", "pru3bA1", "pru3bA1")
+        self.assertEqual(self.validacion.ingresarUsuario("costi.abarca@gmail.com", "pru3bA1"), "Usuario aceptado")
+    
+    
